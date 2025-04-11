6ba5077a0ac60c27d063cf8073e2ccfaa8785c6a
kangyein9892 <inwon9892@naver.com>
2025-04-11 23:51:35 +0900

    feature/#1: mainactivity 시작 설정

--- presentation/src/main/java/org/sopt/at/presentation/main/MainActivity.kt
+++ presentation/src/main/java/org/sopt/at/presentation/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.sopt.at.presentation.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,37 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.at.presentation.signin.SignInActivity
 import org.sopt.at.presentation.ui.theme.AtSoptAndroidTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AtSoptAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AtSoptAndroidTheme {
-        Greeting("Android")
-    }
-}
