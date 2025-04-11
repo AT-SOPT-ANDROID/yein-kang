82a31dddf2b7c25432f9bf41c2cdc6299d681e6b
kangyein9892 <inwon9892@naver.com>
2025-04-11 23:50:53 +0900

    chore/#1: textfield textstyle 수정

--- presentation/src/main/java/org/sopt/at/presentation/component/AtSoptTextField.kt
+++ presentation/src/main/java/org/sopt/at/presentation/component/AtSoptTextField.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -64,6 +65,9 @@ fun AtSoptTextField(
         keyboardOptions = keyboardOptions,
         visualTransformation = visualTransformation,
         interactionSource = interactionSource,
+        textStyle = TextStyle(
+            color = AtSoptTheme.colors.textPrimary
+        ),
         cursorBrush = SolidColor(AtSoptTheme.colors.textPrimary),
         modifier = modifier
             .fillMaxWidth()
