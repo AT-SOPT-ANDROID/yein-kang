d3280b381a48eaaf22d9fedb0d94fce1a9700868
kangyein9892 <inwon9892@naver.com>
2025-04-25 23:56:26 +0900

    feature/#3: userdatasource, userrepostiroy 구현

---
+++ data/src/main/java/org/sopt/at/data/di/RepositoryModule.kt
@@ -0,0 +1,15 @@
+package org.sopt.at.data.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.at.data.repository.UserRepositoryImpl
+import org.sopt.at.repository.UserRepository
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface RepositoryModule {
+    @Binds
+    fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+}
---
+++ data/src/main/java/org/sopt/at/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package org.sopt.at.data.repository
+
+import org.sopt.at.data.datasource.UserDataSource
+import org.sopt.at.entity.SignInEntity
+import org.sopt.at.entity.SignUpEntity
+import org.sopt.at.repository.UserRepository
+import javax.inject.Inject
+
+internal class UserRepositoryImpl @Inject constructor(
+    private val userDataSource: UserDataSource
+) : UserRepository {
+    override fun saveUser(signUpEntity: SignUpEntity) {
+        userDataSource.id = signUpEntity.id
+        userDataSource.password = signUpEntity.password
+    }
+
+    override fun getUser(): SignInEntity {
+        return SignInEntity(
+            id = userDataSource.id,
+            password = userDataSource.password
+        )
+    }
+
+    override fun clearUserPreference() {
+        userDataSource.clearUserPreference()
+    }
+
+}
