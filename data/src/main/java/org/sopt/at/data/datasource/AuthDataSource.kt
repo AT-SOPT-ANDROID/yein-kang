399cc152fc7ff6201c7c5bd85af139541c1883be
kangyein9892 <inwon9892@naver.com>
2025-05-09 03:37:04 +0900

    feature/#7: authDataSource에 signIn 추가

--- data/src/main/java/org/sopt/at/data/datasource/AuthDataSource.kt
+++ data/src/main/java/org/sopt/at/data/datasource/AuthDataSource.kt
@@ -1,5 +1,6 @@
 package org.sopt.at.data.datasource
 
+import org.sopt.at.data.dto.request.SignInRequestDto
 import org.sopt.at.data.dto.request.SignUpRequestDto
 import org.sopt.at.data.service.AuthService
 import javax.inject.Inject
@@ -8,4 +9,6 @@ internal class AuthDataSource @Inject constructor(
     private val authService: AuthService
 ) {
     suspend fun signUp(signUpRequest: SignUpRequestDto) = authService.signUp(signUpRequest)
+
+    suspend fun signIn(signInRequest: SignInRequestDto) = authService.signIn(signInRequest)
 }
