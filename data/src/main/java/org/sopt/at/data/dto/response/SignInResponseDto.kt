e8e073182d5bb4f35d261042ba4f645bbfda6014
kangyein9892 <inwon9892@naver.com>
2025-05-09 03:36:09 +0900

    feature/#7: signIn response, request dto 및 entity 추가

---
+++ data/src/main/java/org/sopt/at/data/dto/response/SignInResponseDto.kt
@@ -0,0 +1,13 @@
+package org.sopt.at.data.dto.response
+
+import kotlinx.serialization.Serializable
+import org.sopt.at.entity.SignInUserEntity
+
+@Serializable
+data class SignInResponseDto(
+    val userId: Int
+) {
+    fun toEntity() = SignInUserEntity(
+        userId = userId
+    )
+}
