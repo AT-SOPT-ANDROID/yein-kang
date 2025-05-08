7accf99083c6d092ae9cc733becbceb39675a486
kangyein9892 <inwon9892@naver.com>
2025-05-09 03:36:35 +0900

    feature/#7: authService에 signIn 함수 추가

--- data/src/main/java/org/sopt/at/data/service/AuthService.kt
+++ data/src/main/java/org/sopt/at/data/service/AuthService.kt
@@ -1,7 +1,9 @@
 package org.sopt.at.data.service
 
+import org.sopt.at.data.dto.request.SignInRequestDto
 import org.sopt.at.data.dto.request.SignUpRequestDto
 import org.sopt.at.data.dto.response.BaseResponseDto
+import org.sopt.at.data.dto.response.SignInResponseDto
 import org.sopt.at.data.dto.response.SignUpResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,5 +13,10 @@ internal interface AuthService {
     suspend fun signUp(
         @Body signUpRequest: SignUpRequestDto
     ): BaseResponseDto<SignUpResponseDto>
+
+    @POST("auth/signin")
+    suspend fun signIn(
+        @Body signInRequest: SignInRequestDto
+    ): BaseResponseDto<SignInResponseDto>
 }
 
