076171e058c7d6643c5ff127254406391f6d86d7
kangyein9892 <inwon9892@naver.com>
2025-05-09 12:27:59 +0900

    chore/#7: authService에 헤더 필요 없으므로 @Tag 추가

--- data/src/main/java/org/sopt/at/data/service/AuthService.kt
+++ data/src/main/java/org/sopt/at/data/service/AuthService.kt
@@ -7,16 +7,19 @@ import org.sopt.at.data.dto.response.SignInResponseDto
 import org.sopt.at.data.dto.response.SignUpResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Tag
 
 internal interface AuthService {
     @POST("auth/signup")
     suspend fun signUp(
-        @Body signUpRequest: SignUpRequestDto
+        @Body signUpRequest: SignUpRequestDto,
+        @Tag authType: AuthType = AuthType.NO_AUTH
     ): BaseResponseDto<SignUpResponseDto>
 
     @POST("auth/signin")
     suspend fun signIn(
-        @Body signInRequest: SignInRequestDto
+        @Body signInRequest: SignInRequestDto,
+        @Tag authType: AuthType = AuthType.NO_AUTH
     ): BaseResponseDto<SignInResponseDto>
 }
 
